a85c477a34cf1033adb2d2eb85b950c992da4dd9
Ronadier <62849569+Ronadier@users.noreply.github.com>
2021-06-05 18:09:34 +0300

    add rest client

--- MessageFacade-ejb/src/main/java/impl/RestClient.java
+++ MessageFacade-ejb/src/main/java/impl/RestClient.java
@@ -1,14 +1,17 @@
 package impl;
 
+import generated.MessageFacadeConfig;
 import service.Message;
 
 public class RestClient {
+    String endpoint = new MessageFacadeConfig().getEndpointREST();
 
     public static String deleteMessage(Message message) {
         return "SUCCESS";
     }
 
     public static String addMessage(Message message) {
+
         return "SUCCESS";
     }
 }
